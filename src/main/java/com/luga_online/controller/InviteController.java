e034eccf9fcfcd616b1f9bdf004d919d5b910655
Evgenii <35301866+qf05@users.noreply.github.com>
2020-03-08 14:19:21 +0300

    redirect_after_login

--- src/main/java/com/luga_online/controller/InviteController.java
+++ src/main/java/com/luga_online/controller/InviteController.java
@@ -1,23 +1,39 @@
 package com.luga_online.controller;
 
+import com.luga_online.model.AuthUser;
+import com.vk.api.sdk.client.VkApiClient;
+import com.vk.api.sdk.exceptions.ApiException;
+import com.vk.api.sdk.exceptions.ClientException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @EnableAutoConfiguration
 public class InviteController {
 
+    @Autowired
+    private VkApiClient vk;
+
     @GetMapping("/invite")
-    public String invitePage() {
-        return "hello";
+    public String user(@AuthenticationPrincipal AuthUser user) {
+        int countFriend = 0;
+        try {
+            countFriend = vk.friends().get(user.getActor()).execute().getCount();
+        } catch (ApiException | ClientException e) {
+            e.printStackTrace();
+        }
+        System.out.println("count friends = " + countFriend);
+        return "count friends = " + countFriend;
     }
 
-    @PostMapping("/invite")
-    public String invite() {
-        return "hello";
-    }
+
+//    @PostMapping("/invite")
+//    public String invite() {
+//        return "hello";
+//    }
 
 
 }
--- src/main/java/com/luga_online/controller/MainController.java
+++ src/main/java/com/luga_online/controller/MainController.java
@@ -1,12 +1,6 @@
 package com.luga_online.controller;
 
-import com.luga_online.model.AuthUser;
-import com.vk.api.sdk.client.VkApiClient;
-import com.vk.api.sdk.exceptions.ApiException;
-import com.vk.api.sdk.exceptions.ClientException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,9 +8,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 @EnableAutoConfiguration
 public class MainController {
-
-    @Autowired
-    private VkApiClient vk;
 
     @GetMapping("/")
     public String main() {
@@ -27,17 +18,5 @@ public class MainController {
     public String mainPost(String uri) {
         System.out.println(uri);
         return "hello";
-    }
-
-    @GetMapping("/user")
-    public String user(@AuthenticationPrincipal AuthUser user) {
-        int countFriend = 0;
-        try {
-            countFriend = vk.friends().get(user.getActor()).execute().getCount();
-        } catch (ApiException | ClientException e) {
-            e.printStackTrace();
-        }
-        System.out.println("count friends = " + countFriend);
-        return "count friends = " + countFriend;
     }
 }
